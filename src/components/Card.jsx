704297923fcc01bbcf6a761bae046a1364c040be
wesines <wes.ines@gmail.com>
2021-02-04 00:50:28 +0100

    display picture

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,24 +12,26 @@ export class Add extends React.Component {
  
   }
   
-  // componentDidMount(){
-  //   fetch(`https://raw.githubusercontent.com/konexio/digitous-assest/main/bakery/${this.props.itemName}`)
-  //   .then(res => res.json())
-  //   .then(resultat => {
-  //       console.log(resultat)
-  //     //this.setState({ image :  });
- 
-  //   })
-  //   .catch(error => console.error(error))
+  componentDidMount(){
+    let url=`https://raw.githubusercontent.com/konexio/digitous-assest/main/bakery/${this.props.itemName}.png`
+    console.log("url",url)
+    fetch(url)
+    .then(res => res.json())
+    .then(resultat => {
+      this.setState({ image :url  });
+    })
+    .catch(error => console.error(error))
   
-  // }
+  }
 
   render() {
+    let source=`https://raw.githubusercontent.com/konexio/digitous-assest/main/bakery/${this.props.itemName}.png`
+   
     return (
         <div>
            <button onClick={(e)=>{this.props.onClick(this.props.itemName,this.props.price)}}>
         
-              <img src={this.state.image}  style={{ width: 100, height:100 }} /></button>
+              <img src={source}  style={{ width: 100, height:100 }} /></button>
 
 
 
